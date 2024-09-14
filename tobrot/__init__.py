075f5a3593dfe2cce09e15bbd57fc5292c9a9cf8
chailana <chailana35217793@gmail.com>
2024-09-14 21:36:33 +0530

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | gautamajay52
-
 import asyncio
 import logging
 import os
@@ -11,15 +7,14 @@ from logging.handlers import RotatingFileHandler
 from sys import exit
 import urllib.request
 import dotenv
-import telegram.ext as tg
 from telegram import Bot
-from pyrogram import Client
+from telegram.ext import Updater
 
 if os.path.exists("CendrawasihLeech.txt"):
     with open("CendrawasihLeech.txt", "r+") as f_d:
         f_d.truncate(0)
 
-# the logging things
+# Logging setup
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
@@ -39,7 +34,7 @@ LOGGER = logging.getLogger(__name__)
 
 dotenv.load_dotenv("config.env")
 
-# checking compulsory variable
+# Check compulsory variables
 for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
     try:
         value = os.environ[imp]
@@ -49,47 +44,36 @@ for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
         LOGGER.critical(f"Oh...{imp} is missing from config.env ... fill that")
         exit()
 
-# The Telegram API things
+# Telegram API setup
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 APP_ID = int(os.environ.get("APP_ID", ""))
 API_HASH = os.environ.get("API_HASH")
 OWNER_ID = int(os.environ.get("OWNER_ID", 1036440597))
 
-# Get these values from my.telegram.org
-# to store the channel ID who are authorized to use the bot
 AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL").split()]
 
-# the download location, where the HTTP Server runs
+# File and configuration settings
 DOWNLOAD_LOCATION = "CendrawasihLeech"
-# Telegram maximum file upload size
 MAX_FILE_SIZE = 2147483648
 TG_MAX_FILE_SIZE = 2147483648
 FREE_USER_MAX_FILE_SIZE = 2147483648
 AUTH_CHANNEL.append(1036440597)
 AUTH_CHANNEL.append(OWNER_ID)
-# chunk size that should be used with requests
 CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", "64"))
-# default thumbnail to be used in the videos
 DEF_THUMB_NAIL_VID_S = os.environ.get(
     "DEF_THUMB_NAIL_VID_S", "https://stikesekaharap-pky.ac.id/wp-content/themes/EHeikra/assets/img/noimage-420x273.jpg"
 )
-# maximum message length in Telegram
 MAX_MESSAGE_LENGTH = 4096
-# set timeout for subprocess
 PROCESS_MAX_TIMEOUT = 7200
-#
 SP_LIT_ALGO_RITH_M = os.environ.get("SP_LIT_ALGO_RITH_M", "hjs")
 ARIA_TWO_STARTED_PORT = int(os.environ.get("ARIA_TWO_STARTED_PORT", "6800"))
 EDIT_SLEEP_TIME_OUT = int(os.environ.get("EDIT_SLEEP_TIME_OUT", "15"))
 MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(
     os.environ.get("MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START", 600)
 )
-MAX_TG_SPLIT_FILE_SIZE = int(os.environ.get(
-    "MAX_TG_SPLIT_FILE_SIZE", "2040109465"))
-# add config vars for the display progress
+MAX_TG_SPLIT_FILE_SIZE = int(os.environ.get("MAX_TG_SPLIT_FILE_SIZE", "2040109465"))
 FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "█")
 UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "░")
-# add offensive API
 TG_OFFENSIVE_API = os.environ.get("TG_OFFENSIVE_API", None)
 CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "")
 LEECH_COMMAND = os.environ.get("LEECH_COMMAND", "leech")
@@ -122,12 +106,12 @@ RENEWME_COMMAND = os.environ.get("RENEWME_COMMAND", "renewme")
 SPEEDTEST = os.environ.get("SPEEDTEST", "speedtest")
 TSEARCH_COMMAND = os.environ.get("TSEARCH_COMMAND", "tshelp")
 BOT_START_TIME = time.time()
-# dict to control uploading and downloading
+
 gDict = defaultdict(lambda: [])
-# user settings dict #ToDo
 user_settings = defaultdict(lambda: {})
 gid_dict = defaultdict(lambda: [])
 _lock = asyncio.Lock()
+
 try:
     RCLONE_CONF_URL = os.environ.get('RCLONE_CONF_URL', "")
     if len(RCLONE_CONF_URL) == 0:
@@ -145,7 +129,7 @@ def multi_rclone_init():
     if not os.path.exists("rclone.conf"):
         LOGGER.warning("Sed, No rclone.conf found in root directory")
         return
-    if not os.path.exists("rclone_bak.conf"):  # backup rclone.conf file
+    if not os.path.exists("rclone_bak.conf"):
         with open("rclone_bak.conf", "w+", newline="\n", encoding="utf-8") as fole:
             with open("rclone.conf", "r") as f:
                 fole.write(f.read())
@@ -153,10 +137,6 @@ def multi_rclone_init():
 
 multi_rclone_init()
 
-# Initialize the Pyrogram client
-app = Client("CendrawasihLeech", bot_token=TG_BOT_TOKEN, api_id=APP_ID, api_hash=API_HASH, workers=343)
-
-# Initialize the Telegram bot updater
 bot = Bot(token=TG_BOT_TOKEN)
-updater = tg.Updater(bot=bot)
+updater = Updater(bot=bot, use_context=True)
 dispatcher = updater.dispatcher
